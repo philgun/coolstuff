4b434db622c200c116174632447086c61514363a
philgun <philipe.gunawan@anu.edu.au>
2021-09-14 10:28:18 +1000

    test commit

--- ML/script/script/effectiveness_based/skopt/run_gp_min.py
+++ ML/script/script/effectiveness_based/skopt/run_gp_min.py
@@ -10,11 +10,11 @@ import tensorflow as tf
 tf.random.set_seed(constant)
 
 import tensorflow.keras as keras
-import keras.regularizers as reg
+import tensorflow.keras.regularizers as reg
 
-from keras.losses import mean_squared_error, mean_absolute_error
-from keras.optimizers import Adam, Nadam, SGD, RMSprop
-from keras.activations import relu, tanh, sigmoid, elu
+from tensorflow.keras.losses import mean_squared_error, mean_absolute_error
+from tensorflow.keras.optimizers import Adam, Nadam, SGD, RMSprop
+from tensorflow.keras.activations import relu, tanh, sigmoid, elu
 from tensorflow.keras.callbacks import EarlyStopping
 
 import numpy as np
@@ -27,7 +27,7 @@ from skopt import BayesSearchCV, gp_minimize
 from skopt.space import Real, Categorical, Integer
 from skopt.utils import use_named_args
 
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, RepeatedKFold, cross_val_score
 from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import reciprocal, uniform, randint
 import joblib
@@ -40,6 +40,7 @@ from tensorflow.keras.models import load_model
 import functools
 
 import argparse
+from sklearn.metrics import r2_score
 
 if __name__ == '__main__':
     #Prep data
@@ -49,10 +50,23 @@ if __name__ == '__main__':
         "--mode", help = "either charging or discharging. Use 'charging' for charging and 'discharging' for discharging", type=str
     )
 
+    parser.add_argument(
+        "--njobs",help = "number of core(s) dedicated to the optimisation algorithm. Put -1 if using all available cores. Default is 1 for 1 core", type=int, default=1
+    )
+
+    parser.add_argument(
+        "--crossval",help = "Run the repeated k-fold cross validation or not. Default 0 for no-cross validation.", type=int, default=0
+    )
+
     args = parser.parse_args()
 
     mode = args.mode
 
+    n_jobs = args.njobs
+
+    crossval = args.crossval
+
+    
     if mode == "discharging":
         fn_train = "/home/philgun/Documents/coolstuff/coolstuff/ML/script/script/effectiveness_based/data/training_data_discharging_equilibrium.csv"
 
@@ -63,57 +77,7 @@ if __name__ == '__main__':
 
         fn_test = "/home/philgun/Documents/coolstuff/coolstuff/ML/script/script/effectiveness_based/data/test_data_charging_equilibrium.csv"
 
-    df = pd.read_csv(fn_train)
-    
-    df_test = pd.read_csv(fn_test)    
-
-    print(df,df_test)
-
-    plt.hist(df.mdot,bins=30)
-    plt.show()
-
-    #Get the X and y from the data set
-    X = df[df.columns[0:-1]].to_numpy()
-    y = df[df.columns[-1]].to_numpy().reshape(-1,1)
-
-    #Split into train validation set
-    X_train, X_valid, y_train, y_valid = train_test_split(X,y,test_size=0.3,random_state=constant)
-
-    #Instantiate scalers
-    mmx = MinMaxScaler().fit(X_train)
-    mmy = MinMaxScaler().fit(y_train)
-    
-    #Normalise the data
-    X_train = mmx.transform(X_train)
-    y_train = mmy.transform(y_train)
-
-    X_valid = mmx.transform(X_valid)
-    y_valid = mmy.transform(y_valid)
-
-    #Normalise the test data
-    X_test = df_test[df_test.columns[0:-1]].to_numpy()
-    y_test = df_test[df_test.columns[-1]].to_numpy().reshape(-1,1)
-    X_test = mmx.transform(X_test)
-
-    assert(
-        X_test.shape[1] == X_train.shape[1]
-    )
-
-    print("Input shape: %s, output shape: %s"%(X_train.shape[1], y_train.shape[1]))
-
-    print(
-        "X_train : \n\n%s"%X_train
-    )
-    print(
-        "y_train : \n\n%s"%y_train
-    )
-    print(
-        "X_test : \n\n%s"%X_test
-    )
-    print(
-        "y_test : \n\n%s"%y_test
-    )
-
+    #Directory to save the model
     fbestmodel = "./GP_Model_%s"%(mode)
 
     dim_first_neuron = Integer(low=3, high=300, name="first_neuron")
@@ -137,64 +101,204 @@ if __name__ == '__main__':
     f.close()
 
     @use_named_args(dimensions=dimensions)
-    def fitness(inputSize=X_train.shape[1], outputSize=y_train.shape[1], first_neuron=30, n_hidden=5, magnifier=1, dropout=0.1, lr=1e-3, first_activation="relu", hidden_activation="relu",last_activation="relu", regulariser=None, l1=1e-3, l2=1e-3):
-        
+    def fitness(first_neuron=30, n_hidden=5, magnifier=1, dropout=0.1, lr=1e-3, first_activation="relu", hidden_activation="relu",last_activation="relu", regulariser=None, l1=1e-3, l2=1e-3):
+        #***************************** Read the last best error
         last_error = np.genfromtxt("./gp_min_best_error_%s.txt"%(mode),dtype=np.float32)
-
-        model = generate_model(
-            inputSize=inputSize,
-            outputSize=outputSize,
-            first_neuron = first_neuron,
-            n_hidden = n_hidden,
-            magnifier = magnifier,
-            dropout = dropout,
-            lr = lr,
-            first_activation=first_activation,
-            hidden_activation=hidden_activation,
-            last_activation=last_activation,
-            regulariser=regulariser,
-            l1=l1,
-            l2=l2
-        )
-
-        #************************** Initialise early stopping
-        monitor = EarlyStopping(
-            monitor="val_mse",
-            mode='min', 
-            min_delta=1e-3,
-            patience=100,
-            restore_best_weights=True
-        )
-
-        history = model.fit(
-            X_train, y_train,
-            epochs=5000,
-            batch_size=int(X_train.shape[0]/400),
-            validation_data=(X_valid, y_valid),
-            callbacks=[monitor]
-        )
-
-        error = min(history.history["val_mse"])
         
-        print(
-            "\nBest error from this iteration: %.6f\nLast error: %s"%(error,last_error)
-        )
+        #Prep the training data
+        df = pd.read_csv(fn_train)    
 
-        if error < last_error:
-            model.save(fbestmodel)
+        if crossval != 0:  
             print(
-                "Save model as %s ..................."%(fbestmodel)
+                "Running the optimisation with cross validation of %s fold.............................."%(crossval)
             )
 
-            f = open("./gp_min_best_error_%s.txt"%(mode),"w")
-            f.write("%s"%(error))
-            f.close()
+            #Get the X and y from the data set
+            X = df[df.columns[0:-1]].to_numpy()
+            y = df[df.columns[-1]].to_numpy().reshape(-1,1)
+
+            #Prep the cross validation data
+            rkf = RepeatedKFold(
+                n_splits=crossval, 
+                n_repeats=10, 
+                random_state=constant
+            )
+
+            #Generate the model for cross validation
+            model = generate_model(
+                inputSize=X.shape[1],
+                outputSize=y.shape[1],
+                first_neuron = first_neuron,
+                n_hidden = n_hidden,
+                magnifier = magnifier,
+                dropout = dropout,
+                lr = lr,
+                first_activation=first_activation,
+                hidden_activation=hidden_activation,
+                last_activation=last_activation,
+                regulariser=regulariser,
+                l1=l1,
+                l2=l2
+            )
+
+            scores = []
+
+            #Train over all the folds
+            for train_index, valid_index in rkf.split(X):
+                X_train, y_train = X[train_index], y[train_index]
+                X_valid, y_valid = X[valid_index], y[valid_index]
+
+                #Instantiate scalers
+                mmx = MinMaxScaler().fit(X_train)
+                mmy = MinMaxScaler().fit(y_train)
+                
+                #Normalise the train-validation data
+                X_train = mmx.transform(X_train)
+                y_train = mmy.transform(y_train)
+
+                X_valid = mmx.transform(X_valid)
+                y_valid = mmy.transform(y_valid)
+
+                #************************** Initialise early stopping
+                monitor = EarlyStopping(
+                    monitor="val_mse",
+                    mode='min', 
+                    min_delta=1e-3,
+                    patience=100,
+                    restore_best_weights=True
+                )
+
+                #************************** Train the model
+                history = model.fit(
+                    X_train, 
+                    y_train,
+                    epochs=5000,
+                    batch_size=int(X_train.shape[0]/400),
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[monitor]
+                )
+
+                #*************************** Append the best score
+                scores.append(
+                    min(history.history["val_mse"])
+              )
+
+            #************************ Calculate the k-fold error based on validation dataset
+            error = sum(scores)/len(scores)
+
+            print(
+                "#############################################################################################\n"
+            )
+            print(
+                "\nBest error from this iteration: %.6f\nLast best error: %s"%(error,last_error)
+            )
+            print(
+                "\n#############################################################################################\n"
+            )
+
+            #*************************** Check if current error is less than the last best error
+            if error < last_error:
+                model.save(fbestmodel)
+                print(
+                    "Save model as %s ..................."%(fbestmodel)
+                )
+                
+                joblib.dump(mmx,"mmx_%s.bin"%(mode))
+                joblib.dump(mmy,"mmy_%s.bin"%(mode))
+                
+                f = open("./gp_min_best_error_%s.txt"%(mode),"w")
+                f.write("%s"%(error))
+                f.close() 
+
+            del model
+
+            keras.backend.clear_session()
+
+            return error
         
-        del model
+        else:
+            print(
+                "Running the optimisation with NO cross validation.............................."
+            )
+            #Get the X and y from the data set
+            X = df[df.columns[0:-1]].to_numpy()
+            y = df[df.columns[-1]].to_numpy().reshape(-1,1)
 
-        keras.backend.clear_session()
+            #Split into train validation set
+            X_train, X_valid, y_train, y_valid = train_test_split(X,y,test_size=0.3,random_state=constant)
 
-        return error
+            #Instantiate scalers
+            mmx = MinMaxScaler().fit(X_train)
+            mmy = MinMaxScaler().fit(y_train)
+            
+            #Normalise the data
+            X_train = mmx.transform(X_train)
+            y_train = mmy.transform(y_train)
+
+            X_valid = mmx.transform(X_valid)
+            y_valid = mmy.transform(y_valid)
+
+            #Generate the model
+            model = generate_model(
+                inputSize=X.shape[1],
+                outputSize=y.shape[1],
+                first_neuron = first_neuron,
+                n_hidden = n_hidden,
+                magnifier = magnifier,
+                dropout = dropout,
+                lr = lr,
+                first_activation=first_activation,
+                hidden_activation=hidden_activation,
+                last_activation=last_activation,
+                regulariser=regulariser,
+                l1=l1,
+                l2=l2
+            )
+
+            #************************** Initialise early stopping
+            monitor = EarlyStopping(
+                monitor="val_mse",
+                mode='min', 
+                min_delta=1e-3,
+                patience=100,
+                restore_best_weights=True
+            )
+
+            #************************ Train the model
+            history = model.fit(
+                X_train, y_train,
+                epochs=5000,
+                batch_size=int(X_train.shape[0]/400),
+                validation_data=(X_valid, y_valid),
+                callbacks=[monitor]
+            )
+
+            #********************** Obtain the best error of this run
+            error = min(history.history["val_mse"])
+            
+            print(
+                "\nBest error from this iteration: %.6f\nLast error: %s"%(error,last_error)
+            )
+
+            #********************** Check error
+            if error < last_error:
+                model.save(fbestmodel)
+                print(
+                    "Save model as %s ..................."%(fbestmodel)
+                )
+
+                joblib.dump(mmx,"mmx_%s.bin"%(mode))
+                joblib.dump(mmy,"mmy_%s.bin"%(mode))
+
+                f = open("./gp_min_best_error_%s.txt"%(mode),"w")
+                f.write("%s"%(error))
+                f.close()
+            
+            del model
+
+            keras.backend.clear_session()
+
+            return error
       
     #Func test
     default_parameters = [
@@ -211,20 +315,20 @@ if __name__ == '__main__':
         0.01
     ]
 
-    #fitness(x=default_parameters) 
-
-    #Run the hyper parameter optimisation
+    #Run the hyperparameter optimisation
     now = time.time()
+    
     print(
         "Start optimisation------------------------------------"
     )
+
     result = gp_minimize(
         func=fitness,
         dimensions=dimensions,
         acq_func = "EI",
         n_initial_points=50,
         x0=default_parameters,
-        n_jobs=4,
+        n_jobs=n_jobs,
         acq_optimizer = "lbfgs",
         random_state=constant
     )
@@ -249,10 +353,71 @@ if __name__ == '__main__':
         "\n#######################################################################\n"
     )
 
+    if crossval != 0:
+        #Prep the training data
+        df = pd.read_csv(fn_train)  
+        X = df[df.columns[0:-1]].to_numpy()
+        y = df[df.columns[-1]].to_numpy().reshape(-1,1)
+        
+        #Split into train validation set
+        X_train, X_valid, y_train, y_valid = train_test_split(
+            X,y,test_size=0.3,random_state=constant
+        )
+
+        #Instantiate scalers
+        mmx = MinMaxScaler().fit(X_train)
+        mmy = MinMaxScaler().fit(y_train)
+        
+        #Normalise the data
+        X_train = mmx.transform(X_train)
+        y_train = mmy.transform(y_train)
+
+        X_valid = mmx.transform(X_valid)
+        y_valid = mmy.transform(y_valid)
+
+        #Params
+        params = result.x
+
+        #Now we have the best configuration from cross val, train the model then
+        model = generate_model(
+            inputSize=X.shape[1],
+            outputSize=y.shape[1],
+            first_neuron = params[0],
+            n_hidden = params[1],
+            magnifier = params[2],
+            dropout = params[3],
+            lr = params[4],
+            first_activation=params[5],
+            hidden_activation=params[6],
+            last_activation=params[7],
+            regulariser=params[8],
+            l1=params[9],
+            l2=[10]
+        )
+
+        #Save the model to fbestmodel file name
+        model.save(fbestmodel)
+
+        #Save the scalers
+        joblib.dump(mmx,"mmx_%s.bin"%(mode))
+        joblib.dump(mmy,"mmy_%s.bin"%(mode))
+
+    #Prep test data
+    df_test = pd.read_csv(fn_test) 
+
+    #Load the scaler
+    mmx = joblib.load("mmx_%s.bin"%(mode))
+    mmy = joblib.load("mmy_%s.bin"%(mode))
+
+    #Normalise the test data
+    X_test = df_test[df_test.columns[0:-1]].to_numpy()
+    y_test = df_test[df_test.columns[-1]].to_numpy().reshape(-1,1)
+    X_test = mmx.transform(X_test)
+
+    #Plot 45 degree plot
     fig,ax = plt.subplots()
     model = load_model(fbestmodel)
     y_pred = mmy.inverse_transform(model.predict(X_test).reshape(-1,1))
-    from sklearn.metrics import r2_score
     
     ax.scatter(
         y_test, y_pred, s= 1, c="red"
@@ -262,7 +427,7 @@ if __name__ == '__main__':
     ax.set_title(
         "Test vs Prediction. R2 : %.6f"%(r2_score(y_test, y_pred))
     )
-    fig.savefig("fig_test_vs_pred_GP.png")
+    fig.savefig("fig_test_vs_pred_GP_%s.png"%(mode))
 
 
     
